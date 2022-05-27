0fecb6205aad280af40c1d4e12b24d2425f03467
lukasz.gancarczyk <lukasz.gancarczyk@idealogic.pl>
2022-05-27 21:33:15 +0200

    Getting title from form

---
+++ RecipeBook.Core/Core/AddRecipeCommand.cs
@@ -0,0 +1,30 @@
+﻿using RecipeBook.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.Core.Core
+{
+    class AddRecipeCommand : CommandBase
+    {
+        private readonly AddRecipeViewModel _addRecipeViewModel;
+        public AddRecipeCommand(AddRecipeViewModel obj)
+        {
+            _addRecipeViewModel = obj;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            //checks if all values are true 
+            return !string.IsNullOrEmpty(_addRecipeViewModel.Title) 
+                && base.CanExecute(parameter);
+        }
+        public override void Execute(object parameter)
+        {
+            Trace.WriteLine($"Its working {_addRecipeViewModel.Title}");
+        }
+    }
+}
--- RecipeBook.Core/ViewModels/AddRecipeViewModel.cs
+++ RecipeBook.Core/ViewModels/AddRecipeViewModel.cs
@@ -1,19 +1,49 @@
-﻿using RecipeBook.Core.Core;
+﻿using Prism.Commands;
+using RecipeBook.Core.Core;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace RecipeBook.Core.ViewModels
 {
     class AddRecipeViewModel: ObservableObject
     {
+        public ICommand AddRecipeCommand { get; }
+
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+
+
+                if (!IsStrEmpty(Title))
+                {
+                    Trace.WriteLine("Error like that");
+                }
+
+                //OnPropertyChanged(nameof(CanCreateReservation));
+            }
+        }
+
+        private bool IsStrEmpty(string str) => !string.IsNullOrEmpty(str);
 
         public AddRecipeViewModel()
         {
-
+            AddRecipeCommand = new AddRecipeCommand(this);
         }
+
+        
     }
 }
